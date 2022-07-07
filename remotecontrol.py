be238377a75d1c641a146a403a3dd56938716d0b
CI\JJohnson <jjohnson@us.ci.org>
2022-07-07 14:50:09 -0400

    post

--- remotecontrol.py
+++ remotecontrol.py
@@ -3,32 +3,37 @@ from http.server import BaseHTTPRequestHandler, HTTPServer
 import time
 import steppercontrol
 
-# curl -X POST http://localhost:8085 -H "Content-Type: application/json" -d '{"direction":"backward", "distance":5}'
 
-
+# Simple http server to receive commands and then call the Stepper Controller to move motor forward or backwards a given number of steps.
+# Request looks like this:
+# curl -X POST http://localhost:8085 -H "Content-Type: application/json" -d '{"direction":"backward", "distance":5, "motor":"alt"}'
+# Stepper motors are dumb, Polar software will need to continue sending commands until it is lined up correctly
 
 class TestPage(BaseHTTPRequestHandler):
     def do_GET(self):
         self.send_response(200)
         self.send_header("Content-type", "text/html")
         self.end_headers()
-        self.wfile.write(bytes("<body><p>This is an example web server.</p></body>", "utf-8"))
+        self.wfile.write(
+            bytes("<body><p>This is just a test page to see if the server is working.</p></body>", "utf-8"))
+
     def do_POST(self):
+        self.send_response(200)
         content_length = int(self.headers['Content-Length'])
         body = self.rfile.read(content_length)
         result = json.loads(body)
         print(result)
         if result['direction'] == "forward":
             print("move forward")
+            steppercontrol.stepforward(result['distance'],result['motor'])
         if result['direction'] == "backward":
             print("move backward")
-        steppercontrol.stepforward()
-        self.send_response(200)
+            steppercontrol.stepbackward(result['distance'], result['motor'])
 
 
-server = HTTPServer(("localhost", 8085),TestPage)
-
+server = HTTPServer(("localhost", 8085), TestPage)
 
 try:
     server.serve_forever()
-except KeyboardInterrupt:pass
+except KeyboardInterrupt:
+    pass
--- steppercontrol.py
+++ steppercontrol.py
@@ -1,19 +1,29 @@
-#sudo pip3 install adafruit-circuitpython-motorkit
+# sudo pip3 install adafruit-circuitpython-motorkit
 import time
 from adafruit_motorkit import MotorKit
 from adafruit_motor import stepper
 
 kit = MotorKit()
 
-def stepforward():
-    for i in range(1000):
-        kit.stepper1.onestep(direction=stepper.FORWARD,style=stepper.DOUBLE)
-        # kit.stepper2.onestep(direction="stepper.BACKWARD")
-        time.sleep(0.01)
+#Basic Stepper Motor Control.
+#Activates a step forward or backwards a given number of steps.
+#az/alt tied to the separate motors.
+#If single motor used alt axis, you must move the alt all the way down first.
 
-def stepbackward():
-    for i in range(1000):
-        kit.stepper1.onestep(direction=stepper.BACKWARD,style=stepper.DOUBLE)
-        # kit.stepper2.onestep(direction="stepper.BACKWARD")
-        time.sleep(0.01)
+def stepforward(n,whichMotor):
+    for i in range(n):
+        if whichMotor == "az":
+            kit.stepper1.onestep(direction=stepper.FORWARD, style=stepper.DOUBLE)
+        if whichMotor == "alt":
+            kit.stepper2.onestep(direction=stepper.FORWARD, style=stepper.DOUBLE)
+        time.sleeo(0.01)
 
+
+
+def stepbackward(n,whichMotor):
+    for i in range(n):
+        if whichMotor == "az":
+            kit.stepper1.onestep(direction=stepper.BACKWARD, style=stepper.DOUBLE)
+        if whichMotor == "alt":
+            kit.stepper2.onestep(direction=stepper.BACKWARD, style=stepper.DOUBLE)
+        time.sleep(0.01)
