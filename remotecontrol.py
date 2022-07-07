5cc8d1f5f6ef9a433fec0c75af241929ef06b61f
CI\JJohnson <jjohnson@us.ci.org>
2022-07-07 13:38:19 -0400

    post

--- remotecontrol.py
+++ remotecontrol.py
@@ -6,13 +6,19 @@ import steppercontrol
 
 
 class TestPage(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+        self.wfile.write(bytes("<body><p>This is an example web server.</p></body>", "utf-8"))
     def do_Post(self):
         content_length = int(self.headers['Content-Length'])
         body = self.rfile.read(content_length)
         print(body)
         steppercontrol.stepforward()
 
-server = HTTPServer(("localhost", 8085),TestPage.do_Post())
+
+server = HTTPServer(("localhost", 8085),TestPage)
 
 
 try:
