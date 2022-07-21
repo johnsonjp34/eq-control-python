ad2007c12128214093e189354bb9c289042452a6
Josh <johnsonjp34@msn.com>
2022-07-20 22:33:44 -0400

    adding correction factor

--- remotecontrol.py
+++ remotecontrol.py
@@ -1,15 +1,16 @@
 import json
 from http.server import BaseHTTPRequestHandler, HTTPServer
-import time
 import steppercontrol
-
 
 # Simple http server to receive commands and then call the Stepper Controller to move motor forward or backwards a given number of steps.
 # Request looks like this:
-# curl -X POST http://localhost:8085 -H "Content-Type: application/json" -d '{"direction":"backward", "distance":5, "motor":"alt"}'
+# curl -X POST http://localhost:8085 -H "Content-Type: application/json" -d '{"direction":"backward", "distance":5, "motor":"alt", "correctionNumber":1}'
 # Stepper motors are dumb, Polar software will need to continue sending commands until it is lined up correctly
 
-class TestPage(BaseHTTPRequestHandler):
+listOfCorrection = []
+
+
+class MotorWebControl(BaseHTTPRequestHandler):
     def do_GET(self):
         self.send_response(200)
         self.send_header("Content-type", "text/html")
@@ -23,17 +24,33 @@ class TestPage(BaseHTTPRequestHandler):
         body = self.rfile.read(content_length)
         result = json.loads(body)
         print(result)
-        if result['direction'] == "forward":
-            print("move forward")
-            steppercontrol.stepforward(result['distance'],result['motor'])
-        if result['direction'] == "backward":
-            print("move backward")
-            steppercontrol.stepbackward(result['distance'], result['motor'])
+        calibration(result['distance', result['motor'], result['correctionNumber'], result['direction']])
 
 
-server = HTTPServer(("localhost", 8085), TestPage)
+server = HTTPServer(("localhost", 8085), MotorWebControl)
 
 try:
     server.serve_forever()
 except KeyboardInterrupt:
     pass
+
+
+def calibration(distance, motor, correctionNumber, direction):
+    listOfCorrection.append(distance)
+
+    if correctionNumber == 1:
+        if direction == "forward":
+            steppercontrol.stepforward(distance, motor)
+        else:
+            steppercontrol.stepbackward(distance, motor)
+
+    if correctionNumber > 1:
+        # correction number begins with 1. this is the error calculated after each set of pictures
+        # correction Factor. This is to see how much the stepper motor is moving the mount
+        # Compare the difference between the last entry and the remaining error and divide by the previous motor steps
+        correctionFactor = (listOfCorrection[correctionNumber - 2] - listOfCorrection[correctionNumber - 1]) / \
+                           listOfCorrection[correctionNumber - 2]
+    if direction == "forward":
+        steppercontrol.stepforward(distance * correctionFactor, motor)
+    else:
+        steppercontrol.stepbackward(distance * correctionFactor, motor)
